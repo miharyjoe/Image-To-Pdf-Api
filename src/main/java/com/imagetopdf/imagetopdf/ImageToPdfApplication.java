9881994c53efeac3fa7302e61885332dc91d1b1e
Mihary Joel <miharyjoel@gmail.com>
2023-04-12 10:43:05 +0300

    feat: initial project

---
+++ src/main/java/com/imagetopdf/imagetopdf/ImageToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.imagetopdf.imagetopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageToPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageToPdfApplication.class, args);
+    }
+
+}
