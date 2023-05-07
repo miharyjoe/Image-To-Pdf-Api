9d8710e0e335324f6787cc1afc3a49202bf8b1ea
miharyjoel <miharyjoel@gmail.com>
2023-05-07 19:00:23 +0300

    feat:  health controller

--- src/main/java/com/imagetopdf/imagetopdf/controller/HealthyController.java
+++ src/main/java/com/imagetopdf/imagetopdf/controller/HealthyController.java
@@ -1,6 +1,5 @@
 package com.imagetopdf.imagetopdf.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
